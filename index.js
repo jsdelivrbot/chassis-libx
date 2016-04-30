6ce3e53d80848721795e38a853962d1f39d437e4
Corey Butler <corey@coreybutler.com>
2016-04-30 17:16:33 -0500

    Updated travis file and added file for node-like environments.

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = function (callback) {
+  if (document) {
+    var s = document.createElement('script')
+    s.setAttribute('type', 'text/javascript')
+    s.setAttribute('src', require('path').join(__dirname, 'dist/chassis.x.min.js'))
+    s.onload = typeof callback === 'function' ? function () { callback() } : function () {}
+    document.head.appendChild(s)
+  } else {
+    console.log('NGNX Chassis failed to load: DOM does not exist or was not recognized.')
+  }
+}
