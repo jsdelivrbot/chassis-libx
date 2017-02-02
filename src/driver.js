12867852ba2d18ab2dd7d6eafb5f0f3ea175ea4c
Corey Butler <corey@coreybutler.com>
2017-02-02 17:05:48 -0600

    Updated NGNX.Driver to leverage NGN.DOM.guarantee when rendering templates.

--- src/driver.js
+++ src/driver.js
@@ -411,6 +411,12 @@ if (!NGN) {
         position = 'beforeend'
       }
 
+      // Setup a mutation observer to guarantee the element is
+      // written to the DOM (if a response is expected)
+      if (NGN.isFn(callback)) {
+        NGN.DOM.guarantee(parent, element, 15000, callback)
+      }
+
       if (typeof element === 'string') {
         parent.insertAdjacentHTML(position, element)
       } else {
@@ -436,10 +442,6 @@ if (!NGN) {
 
         default:
           this.templateRendered(parent.lastChild)
-      }
-
-      if (callback) {
-        callback(element)
       }
     }
 
